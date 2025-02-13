2c79029ce7011cdcc6b8f73484352c0b8f2327bf
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 <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>
2025-02-13 13:31:46 +0530

    commit

--- src/monitor/AgentMonitor.groovy
+++ src/monitor/AgentMonitor.groovy
@@ -1,59 +1,63 @@
-// package monitor
+package monitor
 
-import jenkins.model.*
-import hudson.model.*
-import hudson.node_monitors.*
-import hudson.util.*
+import hudson.model.*;
+import hudson.util.*;
+import jenkins.model.*;
+import hudson.FilePath.FileCallable;
+import hudson.slaves.OfflineCause;
+import hudson.node_monitors.*;
 
 class AgentMonitor {
 
-    // Method to find nodes with free disk space below the given threshold
-    public Map findAllNodesWithLowDiskFreeSpace(minSpaceInGB) {
-        Map mapNodeToFreeSize = [:]
-        Map lowFreeSpaceNodes = [:]
-        def nodesToCheck = Jenkins.instance.nodes  
+  public Map findAllNodesWithLowDiskFreeSpace(minSpaceInGB)
+  {
+    Map mapNodeToFreeSize = [:]
+    Map lowFreeSpaceNodes = [:]
+    def nodesToCheck = Jenkins.instance.nodes  
 
-        for (node in nodesToCheck) {
-            try {
-                def computer = node.toComputer()
-                if (computer.getChannel() == null) continue
+    for (node in nodesToCheck) {
+      println("=== Begin to handle node ${node.name} ===")
+      try {
+        def computer = node.toComputer()
 
-                // Get disk space of the node
-                def diskSpace = DiskSpaceMonitor.DESCRIPTOR.get(computer)
-                def size = 0
-                if (diskSpace != null) {
-                    size = diskSpace.size
-                }
-                def roundedSize = (size / (1024f * 1024f * 1024f)).round(2)
+        if (computer.getChannel() == null) continue
 
-                // Add node to map with rounded disk space
-                mapNodeToFreeSize[node.name.toString()] = roundedSize
-                if (roundedSize < minSpaceInGB) {
-                    lowFreeSpaceNodes[node.name.toString()] = roundedSize
-                }
-            } catch (e) {
-                println("ERROR: exception: " + e.toString())
-                e.printStackTrace()
-            }
+        //Jenkins return null if size is zero.
+        def diskSpace = DiskSpaceMonitor.DESCRIPTOR.get(computer)
+        def size=0
+        if (diskSpace != null) {
+          size = diskSpace.size
         }
+        def roundedSize = (size / (1024f * 1024f * 1024f)).round(2)
 
-        return lowFreeSpaceNodes
+        println("node: " + node.getDisplayName() + ", free space: " + roundedSize + "GB")
+        mapNodeToFreeSize[node.name.toString()] = roundedSize
+        if (roundedSize < minSpaceInGB) {
+          lowFreeSpaceNodes[node.name.toString()] = roundedSize
+        }
+      } catch(e) {
+        println("ERROR: exception: " + e.toString())
+        e.printStackTrace()
+      }
     }
 
-    // Method to generate a description of nodes with low disk space
-    public String generateDescriptionForLowDiskFreeSpaec(minSpaceInGB, lowFreeSpaceNodes) {
+    return lowFreeSpaceNodes
+  }
+
+   public String generateDescriptionForLowDiskFreeSpaec(minSpaceInGB, lowFreeSpaceNodes)
+   {
         def desc = StringBuilder.newInstance()
-        if (lowFreeSpaceNodes.size() > 0) {
+        if(lowFreeSpaceNodes.size() > 0) {
             desc << "<html>"
             desc << "<body>"
             desc << "<h1>Found some nodes, which have free space lower than ${minSpaceInGB}G</h1>"
             desc << "Low Free Space Nodes:"
             desc << "<table border=1>"
-            lowFreeSpaceNodes.each { node, freeSpace ->
-                desc << "<tr>"
-                desc << "<th align='left'>${node}</th>"
-                desc << "<td align='right'><font color=red>${String.format("%.2f", freeSpace)}G</font></td>"
-                desc << "</tr>"
+            lowFreeSpaceNodes.each{node, freeSpace -> 
+              desc << "<tr>"
+              desc <<   "<th align='left'>${node}</th>"
+              desc <<   "<td align='right'><font color=red>${String.format("%.2f",freeSpace)}G</font></td>"
+              desc << "</tr>"
             }
             desc << "</table>"
             desc << "</body>"
@@ -61,5 +65,5 @@ class AgentMonitor {
         }
 
         return desc.toString()
-    }
+   }
 }
